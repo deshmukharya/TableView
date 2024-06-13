d22874dd3eb12692772c81922597add174682d38
Arya Deshmukh <arya.deshmukh@innovapptive.com>
2024-06-13 10:00:01 +0530

    Initial Commit

---
+++ TableView/ViewController.swift
@@ -0,0 +1,84 @@
+//
+//  ViewController.swift
+//  TableView
+//
+//  Created by E5000861 on 31/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController,UITableViewDelegate {
+    
+    
+    @IBOutlet weak var table1 : UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        SetUp()
+        receiveData()
+        // Do any additional setup after loading the view.
+    }
+    
+    var arrayOfResults = [DataManager]()
+    
+    func SetUp() {
+        table1.rowHeight = UITableView.automaticDimension
+        table1.delegate = self
+        table1.dataSource = self
+    }
+    func receiveData(){
+        arrayOfResults = TableViewData.receiveData()
+        print(arrayOfResults)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        let data = arrayOfResults[indexPath.row]
+        switch data.type{
+        case .text:
+            return 100
+        case .images:
+            return 150
+        case .imageWithTextModel:
+            return 100
+        }
+        
+    }
+    
+}
+
+extension ViewController : UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        arrayOfResults.count
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+    let data = arrayOfResults[indexPath.row]
+    switch data.type{
+    case .text:
+        guard let cell =  tableView.dequeueReusableCell(withIdentifier:TextTableViewCell.identifier ) as? TextTableViewCell else {
+                return UITableViewCell(frame: .zero)
+            }
+            print("Text Section")
+            cell.setData(dataModel: data as! Text)
+            return cell
+        case .images:
+            guard let cell =  tableView.dequeueReusableCell(withIdentifier:ImageTableViewCell.imageIdentifier ) as? ImageTableViewCell else {
+                return UITableViewCell(frame: .zero)
+            }
+            cell.setData(dataModel: data as! Images)
+            print("Image Section")
+            return cell
+        
+       case .imageWithTextModel:
+             guard let cell =  tableView.dequeueReusableCell(withIdentifier:ImageandTextViewCell.cellIdentifier ) as? ImageTableViewCell else {
+            return UITableViewCell(frame: .zero)
+             }
+             cell.setData(dataModel: data as! Images)
+             print("Image Section")
+             return cell
+    }
+    }
+}
